f0a6131fc2d56ffe7bf82019757235adf7cf5586
Romainv78 <romainvaladon@gmail.com>
2022-02-09 16:29:16 +0100

    nav vidéo des lightbox ok

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -1,19 +1,19 @@
 //Factory qui traite l'affichage des médias d'un photographe
 function mediaFactory(data) {
     const { id, photographerId, video, title, image, likes, date, price, alt } = data;
-    const media = `./assets/photographers/${photographerId}/${image}`;
 
     function MediaDOM() {
         const mediaList = document.createElement("div");
         mediaList.setAttribute("class","media_card");
 
         let card = "";
-        card += `<a href="${media}" data-mediaid="${id}" class="medias">`;
 
         if (image !== undefined) {
-            card += `<img src="${media}" alt="${alt}">`;
+            
+            card += `<a href="./assets/photographers/${photographerId}/${image}" data-mediaid="${id}" class="medias">
+            <img src="./assets/photographers/${photographerId}/${image}" alt="${alt}">`;
         } else if (video !== undefined) {
-            card += `
+            card += `<a href="./assets/photographers/${photographerId}/${video}" data-mediaid="${id}" class="medias">
             <video>
                 <source src="./assets/photographers/${photographerId}/${video}" type="video/mp4">
             </video>`;   
--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -1,51 +1,199 @@
-//import {enableBodyScroll, disableBodyScroll} from "./body-scroll-lock"
+//gère la création de lightbox en fonction du tri des médias
+async function Lightbox() {
+    const photographerMedia = await getPhotographers();
 
-class lightbox {
-    //stocke les medias dans une array et appel de la fonction de création de la lightbox quand on clique sur un des média
-    static init() {
-        console.log("init")
-        const links = Array.from(document.querySelectorAll("medias"))
-        console.log(links)
-        const gallery = links.map(link => link.getAttribute("href"))
-        links.forEach(link => link.addEventListener("click", e => {
-                e.preventDefault()
-                new lightbox(e.currentTarget.getAttribute('href'), gallery)
-            }))
-    }
+    LightboxCreate()
 
-    // appel le DOM et le loader de l'image
-    constructor(url, gallery) {
-        this.element = this.buildDOM(url)
-        this.images = gallery
-        this.loadImage(url)
-        this.onKeyUp = this.onKeyUp.bind(this)
-        document.body.appendChild(this.element)
-        //disableBodyScroll(this.element)
-        document.addEventListener("keyup", this.onKeyUp.bind(this))
-    }
+    filter.addEventListener("click", () => {
+        LightboxCreate()
+    })
 
-    //gère le chargement de l'image avec une animation et l'affiche une fois chargé
-    loadImage(url) {
-        this.url = null
-        const image = new Image()
-        const container = this.element.querySelector(".lightbox-container")
-        const loader = document.createElement("div")
-        loader.classList.add("lightbox-loader")
-        container.innerHTML = ''
-        container.appendChild(loader)
-        image.onload = () => {
-            console.log("chargé")
-            container.removeChild(loader)
-            container.appendChild(image)
-            this.url = url
+}
+
+let links = "";
+
+//récupère les images dans la page et écoute quand on clique sur l'une d'entre elles
+function LightboxCreate() {
+
+    //récupère les images dans la page 
+    links = document.querySelectorAll('a[href$=".jpg"], a[href$=".mp4"]')
+
+
+    //écoute quand on clique sur l'une d'entre elles et récupère les éléments créés
+    links.forEach(link => link.addEventListener('click', e => {
+        e.preventDefault()
+        constructor(e.currentTarget.getAttribute('href'), e.currentTarget)
+        closeButton = document.querySelector(".lightbox-close");
+        prevButton = document.querySelector(".lightbox-prev");
+        nextButton = document.querySelector(".lightbox-next");
+        lightbox = document.querySelector(".lightbox");
+        naviguation(e.currentTarget);
+    }))
+}
+
+//permet de fermer la lightbox et passer d'un média à un autre
+function naviguation(media) {
+    var mediaType = media.getAttribute("href").slice(media.getAttribute("href").length - 4); //récupère les 4 dernièrs caractères de l'url du média
+    let mediaArray = Array.from(links); //converti la nodelist en array
+    let cachedPath = "";
+
+    //ferme la lightbox
+    closeButton.addEventListener("click", () => {
+        lightbox.style.display = "none";
+        lightbox.remove();
+    })
+
+    prevButton.addEventListener("click", () => {
+        let i = "";
+        let mediaLightbox = "";
+        let mediaContainer = document.querySelector(".lightbox-container");
+        let mediaUrl = "";
+        let mediaCache = "";
+
+        if (mediaType === ".mp4") { //si l'url du média fini par .mp4 alors on récupère l'url via l'objet html correspondant et on attribu son url à mediaUrl
+            let lightboxVideo = mediaContainer.querySelector("video").querySelector("source");
+            mediaLightbox = mediaContainer.querySelector("video");
+            mediaUrl = lightboxVideo.getAttribute("src");
+            mediaCache = mediaUrl.substring(1);
         }
-        image.src = url
+
+        if (mediaType === ".jpg") {
+            let lightboxImage = mediaContainer.querySelector("img");
+            mediaLightbox = lightboxImage;
+            mediaUrl = lightboxImage.getAttribute("src");
+            mediaCache = mediaUrl.substring(1);
+        }
+
+        //cherche l'url dans la liste des média et retourne son id
+        i = mediaArray.findIndex(element => element.pathname === mediaCache);
+
+        //si on est sur le premier média, renvois au dernier
+        if (i === 0) {
+            cachedPath = mediaArray[mediaArray.length - 1].getAttribute("href"); //obtiens l'url du média précédent dans la liste
+            mediaType = cachedPath.slice(cachedPath.length - 4); //récupère les 4 dernièrs caractères de l'url
+
+            if (mediaType === ".jpg") {  //si l'url fini par .jpg alors créer une image
+                mediaContainer.innerHTML = `
+                    <img src="${cachedPath}" alt="">`;
+            }
+            else if (mediaType === ".mp4") {  //si l'url fini par .mp4 alors créer une vidéo
+                mediaContainer.innerHTML = `<video controls>
+                    <source src="${cachedPath}" type="video/mp4">
+                    </video>`;
+            }
+
+            mediaLightbox.remove();
+        }
+
+        //sinon renvois juste au précédent
+        else if (i !== 0) {
+            cachedPath = mediaArray[i - 1].getAttribute("href");
+            mediaType = cachedPath.slice(cachedPath.length - 4);
+            if (mediaType === ".jpg") {
+                mediaContainer.innerHTML = `
+                    <img src="${cachedPath}" alt="">`;
+            }
+            else if (mediaType === ".mp4") {
+                mediaContainer.innerHTML = `<video controls>
+                    <source src="${cachedPath}" type="video/mp4">
+                    </video>`;
+            }
+
+            mediaLightbox.remove();
+        }
+    })
+
+    nextButton.addEventListener("click", () => {
+        let i = "";
+        let mediaLightbox = "";
+        let mediaContainer = document.querySelector(".lightbox-container");
+        let mediaUrl = "";
+        let mediaCache = "";
+
+        if (mediaType === ".mp4") { //si l'url du média fini par .mp4 alors on récupère l'url via l'objet html correspondant et on attribu son url à mediaUrl
+            let lightboxVideo = mediaContainer.querySelector("video").querySelector("source");
+            mediaLightbox = mediaContainer.querySelector("video");
+            mediaUrl = lightboxVideo.getAttribute("src");
+            mediaCache = mediaUrl.substring(1);
+        }
+
+        if (mediaType === ".jpg") {
+            let lightboxImage = mediaContainer.querySelector("img");
+            mediaLightbox = lightboxImage;
+            mediaUrl = lightboxImage.getAttribute("src");
+            mediaCache = mediaUrl.substring(1);
+        }
+
+        //cherche l'url dans la liste des média et retourne son id
+        i = mediaArray.findIndex(element => element.pathname === mediaCache);
+
+
+        if (i === mediaArray.length - 1) {
+            cachedPath = mediaArray[0].getAttribute("href"); //obtiens l'url du média précédent dans la liste
+            mediaType = cachedPath.slice(cachedPath.length - 4); //récupère les 4 dernièrs caractères de l'url
+
+            if (mediaType === ".jpg") {  //si l'url fini par .jpg alors créer une image
+                mediaContainer.innerHTML = `
+                    <img src="${cachedPath}" alt="">`;
+            }
+            else if (mediaType === ".mp4") {  //si l'url fini par .mp4 alors créer une vidéo
+                mediaContainer.innerHTML = `<video controls>
+                    <source src="${cachedPath}" type="video/mp4">
+                    </video>`;
+            }
+
+            mediaLightbox.remove();
+
+        } else if (i !== mediaArray.length) {
+
+            cachedPath = mediaArray[i + 1].getAttribute("href");
+            mediaType = cachedPath.slice(cachedPath.length - 4); //récupère les 4 dernièrs caractères de l'url
+
+            if (mediaType === ".jpg") {
+                mediaContainer.innerHTML = `
+                    <img src="${cachedPath}" alt="">`;
+            }
+            else if (mediaType === ".mp4") {
+                mediaContainer.innerHTML = `<video controls>
+                    <source src="${cachedPath}" type="video/mp4">
+                    </video>`;
+            }
+
+            mediaLightbox.remove();
+
+        }
+    })
+}
+
+//assigne la lightbox au document avec pour donnée l'image sur laquelle on a cliqué
+function constructor(url, media) {
+    const element = this.lightboxDOM(url, media);
+    const child = document.querySelector(".scripts");
+    document.body.insertBefore(element, child);
+}
+
+//créer la lightbox
+function lightboxDOM(url, media) {
+    const mediaType = media.getAttribute("href").slice(media.getAttribute("href").length - 4);
+    const dom = document.createElement("div");
+    dom.classList.add("lightbox");
+
+    if (mediaType === ".jpg") {
+        dom.innerHTML = `<button class="lightbox-close">
+        <i class="fas fa-times lightbox-exit"></i>
+    </button>
+    <button class="lightbox-prev">
+        <i class="fas fa-chevron-left lightbox-nav"></i>
+    </button>
+    <button class="lightbox-next">
+        <i class="fas fa-chevron-right lightbox-nav"></i>
+    </button>
+    <div class="lightbox-container">
+        <img src="${url}" alt="">
+    </div>`;
     }
 
-    //Créé la lightbox en fonction de l'image cliqué et gère la naviguation
-    buildDOM(url) {
-        const dom = document.createElement("div")
-        dom.classList.add("lightbox")
+    else if (mediaType === ".mp4") {
         dom.innerHTML = `<button class="lightbox-close">
             <i class="fas fa-times lightbox-exit"></i>
         </button>
@@ -56,57 +204,11 @@ class lightbox {
             <i class="fas fa-chevron-right lightbox-nav"></i>
         </button>
         <div class="lightbox-container">
-            
-        </div>`
-        dom.querySelector('.lightbox__close').addEventListener('click', this.close.bind(this))
-        dom.querySelector('.lightbox__next').addEventListener('click', this.next.bind(this))
-        dom.querySelector('.lightbox__prev').addEventListener('click', this.prev.bind(this))
-        return dom
+        <video controls>
+            <source src="${url}" type="video/mp4">
+        </video>
+        </div>`;
     }
 
-    //ferme la lightbox via le bouton + clavier
-    close(e) {
-        e.preventDefault()
-        this.element.classList.add("fadeout")
-        //enableBodyScroll(this.element)
-        window.setTimeout(() => {
-            this.element.parentElement.removeChild(this.element)
-        }, 500)
-        document.removeEventListener("keyup", this.onKeyUp)
-    }
-
-    //va à l'image suivante via le bouton + clavier
-    next(e) {
-        e.preventDefault()
-        let i = this.images.findIndex(image => image === this.url)
-        if (i === this.images.length - 1) {
-            i = -1
-        }
-        this.loadImage(this.images[i + 1])
-    }
-
-    //va à l'image précédente via le bouton + clavier
-    prev(e) {
-        e.preventDefault()
-        let i = this.images.findIndex(image => image === this.url)
-        if (i === 0) {
-            i = this.images.length
-        }
-        this.loadImage(this.images[i - 1])
-    }
-
-    //gère les input du clavier
-    onKeyUp (e) {
-        if (e.key == "escape") {
-            this.close(e)
-        } else if (e.key == "ArrowLeft") {
-            this.prev(e)
-        } else if (e.key == "ArrowRight") {
-            this.next(e)
-        }
-    }
-
+    return dom;
 }
-
-
-lightbox.init()
